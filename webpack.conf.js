8e152b0ec7ae0f4d9481302e9790d313d0fda635
Fernando Marmolejo <fmarvallejo@gmail.com>
2019-05-25 00:56:50 +0200

    New website

--- webpack.conf.js
+++ webpack.conf.js
@@ -6,10 +6,11 @@ export default {
     loaders: [
       {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=/[hash].[ext]"
+        loader: "file?name=/[hash].[ext]"
       },
+      {test: /\.json$/, loader: "json-loader"},
       {
-        loader: "babel-loader",
+        loader: "babel",
         test: /\.js?$/,
         exclude: /node_modules/,
         query: {cacheDirectory: true}
@@ -19,17 +20,16 @@ export default {
 
   plugins: [
     new webpack.ProvidePlugin({
-      "fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+      "fetch": "imports?this=>global!exports?global.fetch!whatwg-fetch"
     })
   ],
 
   context: path.join(__dirname, "src"),
   entry: {
-    app: ["./js/app"],
-    cms: ["./js/cms"]
+    app: ["./js/app"]
   },
   output: {
-    path: path.join(__dirname, "dist"),
+    path: path.join(__dirname, "dist/"),
     publicPath: "/",
     filename: "[name].js"
   },
