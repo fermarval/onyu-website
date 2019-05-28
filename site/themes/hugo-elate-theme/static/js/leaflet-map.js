f93d4cbc410c8ee8ddc5a47391855ef308035adb
Fernando Marmolejo <fmarvallejo@gmail.com>
2019-05-28 14:24:55 +0200

    Fix map v4

--- site/themes/hugo-elate-theme/static/js/leaflet-map.js
+++ site/themes/hugo-elate-theme/static/js/leaflet-map.js
@@ -2,7 +2,7 @@ function initMap() {
     var latLong1 = [37.375433, -5.990077]; // Wiesbaden
     var latLong2 = [39.407899, -0.431551]; // Wiesbaden
 
-    var map = L.map("map").setView(latLong, 5);
+    var map = L.map("map").setView(latLong1, 5);
 
     L.tileLayer("https://b.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
