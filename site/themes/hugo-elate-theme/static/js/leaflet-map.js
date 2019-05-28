402ddb149e8fb4a442d4a1f4df49cf50b1f00a08
Fernando Marmolejo <fmarvallejo@gmail.com>
2019-05-28 14:11:14 +0200

    Test map 2

--- site/themes/hugo-elate-theme/static/js/leaflet-map.js
+++ site/themes/hugo-elate-theme/static/js/leaflet-map.js
@@ -1,7 +1,7 @@
 function initMap() {
-    var latLong = [39.4078969,-0.431551]; // Wiesbaden
+    var latLong = [50.084750, 8.247026]; // Wiesbaden
 
-    var map1 = L.map("map1").setView(latLong, 5);
+    var map = L.map("map").setView(latLong, 5);
 
     L.tileLayer("https://b.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
